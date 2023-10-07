a9e9e7015800b7f4dcd023a48a5f9df9ca7a364e
gyoon <inticoy0406@gmail.com>
2023-10-07 16:42:13 +0900

    Fix: random.c Norm

--- srcs/utils/random.c
+++ srcs/utils/random.c
@@ -3,17 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   random.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jukim2 <jukim2@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:13:42 by kjs               #+#    #+#             */
-/*   Updated: 2023/10/05 17:30:42 by jukim2           ###   ########.fr       */
+/*   Updated: 2023/10/07 16:27:28 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-double	random_double() 
-{    
+double	random_double()
+{
 	// return ((double)rand() * 2 / (double)RAND_MAX - 1);
 	static unsigned int seed;
 
@@ -21,18 +19,18 @@ double	random_double()
 		seed = 12345;
 	seed = (unsigned int)(seed * 1664525 + 1013904223);
     double random_value = (double)seed / (unsigned int)4294967295;
-	
-    return 2.0 * random_value - 1.0;
+
+	return (2.0 * random_value - 1.0);
 }
 
-double	random_double_zerone() 
-{    
-	static unsigned int seed;
+double	random_double_zerone()
+{
+	static unsigned int	seed;
 
 	if (seed == 0)
 		seed = 12345;
 	seed = (unsigned int)(seed * 1664525 + 1013904223);
-    double random_value = (double)seed / (unsigned int)4294967295;
-	
-    return random_value;
+	double random_value = (double)seed / (unsigned int)4294967295;
+
+	return random_value;
 }
