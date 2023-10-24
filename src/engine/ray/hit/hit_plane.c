56d3f6d72a694e4744738cc51a6f0ee11270285b
inticoy <inticoy0406@gmail.com>
2023-10-24 17:28:44 +0900

    Feat: hit_plane fix return value
    - t < 0, returns is_hit = FALSE

--- src/engine/ray/hit/hit_plane.c
+++ src/engine/ray/hit/hit_plane.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 19:02:19 by gyoon             #+#    #+#             */
-/*   Updated: 2023/10/11 14:22:24 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/10/24 17:10:57 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,18 @@
 t_hit_record	hit_plane(t_ray ray, t_shape *shape)
 {
 	t_hit_record	record;
-	t_vec3	o_c;
-	double	n_dot_c;
-	double	n_dot_d;
+	t_vec3			o_c;
+	double			t;
 
 	init_hit_record(&record);
 	o_c = sub_vec3(ray.origin, shape->coord);
-	n_dot_c = dot_vec3(shape->form_vector, sub_vec3(shape->coord, ray.origin));
-	n_dot_d = dot_vec3(shape->form_vector, ray.direction);
-	if (n_dot_c / n_dot_d == 0)
+	t = dot_vec3(shape->form_vector, sub_vec3(shape->coord, ray.origin)) \
+		/ dot_vec3(shape->form_vector, ray.direction);
+	if (compare_double(t, 0.) <= 0)
 		return (record);
 	record.is_hit = TRUE;
 	record.is_front = TRUE;
-	record.t = n_dot_c / n_dot_d;
+	record.t = t;
 	record.hit_shape = shape;
 	record.point = add_vec3(ray.origin, scale_vec3(record.t, ray.direction));
 	record.color = shape->rgb;
