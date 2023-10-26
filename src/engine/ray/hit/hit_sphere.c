780297af001985c30cc8127c2e93165daf04c766
inticoy <inticoy0406@gmail.com>
2023-10-26 22:56:41 +0900

    Feat: hit circle

--- src/engine/ray/hit/hit_sphere.c
+++ src/engine/ray/hit/hit_sphere.c
@@ -6,13 +6,26 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 16:42:43 by gyoon             #+#    #+#             */
-/*   Updated: 2023/10/11 14:11:01 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/10/24 17:27:41 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray.h"
 #include "numft.h"
 #include <math.h>
+
+static double	get_minimum_t(double a, double b, double c)
+{
+	double	t1;
+	double	t2;
+
+	t1 = (-b + sqrt(b * b - a * c)) / a;
+	t2 = (-b - sqrt(b * b - a * c)) / a;
+	if (compare_double(t2, 0.) <= 0)
+		return t1;
+	else
+		return t2;
+}
 
 t_hit_record	hit_sphere(t_ray ray, t_shape *shape)
 {
@@ -21,18 +34,18 @@ t_hit_record	hit_sphere(t_ray ray, t_shape *shape)
 	double			a;
 	double			b;
 	double			c;
-	double			d;
 
 	init_hit_record(&record);
 	oc = sub_vec3(ray.origin, shape->coord);
 	a = dot_vec3(ray.direction, ray.direction);
 	b = dot_vec3(oc, ray.direction);
 	c = dot_vec3(oc, oc) - (shape->diameter / 2.) * (shape->diameter / 2.);
-	d = b * b - a * c;
-	if (d < 0)
+	if (compare_double((b * b - a * c), 0.) <= 0)
+		return (record);
+	else if (compare_double((-b + sqrt(b * b - a * c)) / a, 0.) < 0)
 		return (record);
 	record.is_hit = TRUE;
-	record.t = (-b - sqrt(d)) / a;
+	record.t = get_minimum_t(a, b, c);
 	record.hit_shape = shape;
 	record.point = add_vec3(ray.origin, scale_vec3(record.t, ray.direction));
 	record.normal = norm_vec3(sub_vec3(record.point, shape->coord));
