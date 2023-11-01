9b8d91ff23dae6da334598b390db5d41be13b9d8
inticoy <inticoy0406@gmail.com>
2023-11-01 14:34:05 +0900

    Refactor: Get_color

--- src/engine/color/get_color.c
+++ src/engine/color/get_color.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 11:13:05 by jukim2            #+#    #+#             */
-/*   Updated: 2023/11/01 13:41:18 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/11/01 14:00:24 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,29 +36,30 @@ t_vec3	get_specular_color(t_objects *objs, t_hit_record r)
 
 int	get_color(t_engine *e, int x, int y)
 {
-	t_ray	ray;
-	int		sampling;
-	t_vec3	color_vector_sum;
+	int				sampling;
+	t_vec3			rgb_sum;
+	t_ray			ray;
+	t_vec3			offset;
+	t_hit_record	r;
 
 	sampling = 0;
-	color_vector_sum = vec3(0, 0, 0);
+	rgb_sum = vec3(0, 0, 0);
 	ray.origin = e->objs.camera.coord;
 	while (sampling < SAMPLE_CNT)
 	{
-		t_vec3 offset = add_vec3(scale_vec3(x + rand_double(-0.5, 0.5), e->display.px_dt[WD]), \
-								scale_vec3(y + rand_double(-0.5, 0.5), e->display.px_dt[HT]));
+		offset = add_vec3(scale_vec3(x + rand_double(-0.5, 0.5), e->display.px_dt[WD]), scale_vec3(y + rand_double(-0.5, 0.5), e->display.px_dt[HT]));
 		ray.direction = norm_vec3(add_vec3(e->display.bot_lt_px, offset));
-		color_vector_sum = add_vec3(color_vector_sum, raytrace(ray, &e->objs, 20));
+		rgb_sum = add_vec3(rgb_sum, raytrace(ray, &e->objs, 20));
 		sampling++;
 	}
-	color_vector_sum = scale_vec3(1. / SAMPLE_CNT, color_vector_sum);
-	t_hit_record r = get_hit_record(ray, e->objs.shape);
+	rgb_sum = scale_vec3(1. / SAMPLE_CNT, rgb_sum);
+	r = get_hit_record(ray, e->objs.shape);
 	if (r.is_hit)
 	{
-		color_vector_sum = add_vec3(color_vector_sum, scale_vec3(e->objs.ambient_light.ambient, r.rgb));
-		correct_color(&color_vector_sum, r.rgb);
-		color_vector_sum = add_vec3(color_vector_sum, get_specular_color(&e->objs, r));
-		correct_color(&color_vector_sum, r.rgb);
+		rgb_sum = add_vec3(rgb_sum, scale_vec3(e->objs.ambient_light.ambient, r.rgb));
+		correct_color(&rgb_sum, r.rgb);
+		rgb_sum = add_vec3(rgb_sum, get_specular_color(&e->objs, r));
+		correct_color(&rgb_sum, r.rgb);
 	}
-	return (convert_color_vec3(color_vector_sum));
+	return (convert_color_vec3(rgb_sum));
 }
