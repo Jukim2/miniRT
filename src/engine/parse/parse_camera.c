c29026d27acebbae2a807f7e30829d33b15e15ea
inticoy <inticoy0406@gmail.com>
2023-11-03 15:13:09 +0900

    Fix: parse_camera

--- src/engine/parse/parse_camera.c
+++ src/engine/parse/parse_camera.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 13:13:41 by jukim2            #+#    #+#             */
-/*   Updated: 2023/11/03 14:13:32 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/11/03 15:12:02 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,21 @@ void	parse_camera(t_objects *objs, char *line)
 		(objs->camera.forward.y < -1 || objs->camera.forward.y > 1) || \
 		(objs->camera.forward.z < -1 || objs->camera.forward.z > 1))
 	{
-		printf("Error\nCamera Orientation Vector should have values in [-1,1]\
-				for each x y z axis\n");
+		printf("Error\nCamera Orientation Vector must have values in [-1,1]\n");
 		clean_program(objs, line);
 	}
 	else if (doublecmp(vec3len(objs->camera.forward), 0.) <= 0)
 	{
-		printf("Error\ncamera orientation vector should have length\n");
+		printf("Error\nCamera Orientation Vector should have length\n");
 		clean_program(objs, line);
 	}
 	objs->camera.forward = norm_vec3(objs->camera.forward);
 	idx = parse_double(&objs->camera.fov, line, \
 	check_misconfig(objs, line, idx));
+	if (objs->camera.fov < 0 || objs->camera.fov > 180)
+	{
+		printf("Error\nCamera field of view should be in [-1,1]\n");
+		clean_program(objs, line);
+	}
 	check_endconfig(objs, line, idx);
 }
