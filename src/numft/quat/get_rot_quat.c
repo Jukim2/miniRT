c2eff76b6a5caf186034763b8970bc73d820867b
inticoy <inticoy0406@gmail.com>
2023-11-04 02:12:58 +0900

    Del: comments

--- src/numft/quat/get_rot_quat.c
+++ src/numft/quat/get_rot_quat.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:05:39 by gyoon             #+#    #+#             */
-/*   Updated: 2023/11/03 22:39:05 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/11/04 02:12:48 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,6 @@ t_quat	get_rot_quat(t_vec3 u, t_vec3 v)
 	c = cross_vec3(u, v);
 	if (doublecmp(vec3len(c), 0) != 0)
 		c = norm_vec3(c);
-	printf("cross: ");
-	print_vec3(c);
 	cos = dot_vec3(u, v);
 	if (doublecmp(cos, 1.) == 0)
 		return (quat(1, 0, 0, 0));
@@ -35,7 +33,6 @@ t_quat	get_rot_quat(t_vec3 u, t_vec3 v)
 		return (quat(0, 1, 0, 0));
 	sin_half = sqrt((1.0 - cos) / 2.0);
 	cos_half = sqrt((1.0 + cos) / 2.0);
-	// printf("cos half: %.40f\n", cos);
 	q = quat(cos_half, c.x * sin_half, c.y * sin_half, c.z * sin_half);
 	q = norm_quat(q);
 	return (q);
