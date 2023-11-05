cc4f99e63b84316e2e6b1dbbaf7c2731dd0a7892
Jukim2 <junseojskim@gmail.com>
2023-11-05 11:09:26 +0900

    Fix: Destroy mlx ptrs

--- src/engine/quit_engine.c
+++ src/engine/quit_engine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quit_engine.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jukim2 <jukim2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 17:13:03 by gyoon             #+#    #+#             */
-/*   Updated: 2023/11/04 17:24:27 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/11/05 11:07:18 by jukim2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	quit_engine(void *ptr)
 	t_engine	*e;
 
 	e = (t_engine *)ptr;
-	mlx_clear_window(e->mlx, e->win);
+	mlx_destroy_window(e->mlx, e->win);
+	mlx_destroy_image(e->mlx, e->img.ptr);
 	clean_engine(&e->objs, 0);
 	exit(0);
 }
